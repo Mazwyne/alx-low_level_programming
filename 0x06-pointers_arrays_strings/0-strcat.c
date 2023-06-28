ab634bc7b8e68d6e55fb81d55a230fd7289633f0
Mazwyne <tdkmazwane@gmail.com>
2023-06-28 12:23:52 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,11 @@
 /**
  * _strcat - concatenates two strings
  * @dest: input value
- * @src:input value
+ * @src: input value
  * Return: void
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
